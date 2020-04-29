0163eb2ca27e398cd0b125ca13774d57b4d95546
AdamariMosqueda <adamari.mosqueda16@tectijuana.edu.mx>
2020-04-29 12:28:32 -0700

    Segunda pantalla

--- ExU2/src/app/app-routing.module.ts
+++ ExU2/src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'recomendaciones',
+    loadChildren: () => import('./recomendaciones/recomendaciones.module').then( m => m.RecomendacionesPageModule)
+  },
 ];
 
 @NgModule({
